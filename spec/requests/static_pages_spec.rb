caafd7488ef8829636257b1cf3f8919ce223ad8d
Ryan Thames <ryanthames@gmail.com>
2013-03-29 15:07:16 -0500

    Add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,17 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	let(:base_title) {"Home Inventory Manager"}
+
   describe "Home page" do
     it "should have the content 'Home Inventory Manager'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home Inventory Manager')
+      page.should have_selector('h1', :text => "#{base_title}")
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Home Inventory Manager | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -22,7 +24,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Home Inventory Manager | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -34,7 +36,19 @@ describe "StaticPages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Home Inventory Manager | About")
+      page.should have_selector('title', :text => "#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
   end
 end
