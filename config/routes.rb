fb94df24f94d0af339dfd5ba86a04eaf4dca4c60
Ryan Thames <ryanthames@gmail.com>
2013-04-02 16:25:29 -0500

    Add Users controller; Add routing for signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 HomeInventory::Application.routes.draw do
 
+  get "users/new"
+
   root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
 
   match '/help',    to: 'static_pages#help' #also creates named route called 'help_path'
   match '/about',   to: 'static_pages#about'
